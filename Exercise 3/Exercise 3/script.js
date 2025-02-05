38478d376563fe1149cf763cbe51d9c66145368a
BOOG77 <dawsonb14@hotmail.com>
2025-02-04 20:10:55 -0400

    exercise 3

--- Exercise 3/Exercise 3/script.js
+++ Exercise 3/Exercise 3/script.js
@@ -1,4 +1,5 @@
 // Part 1: Arithmetic Operators
+console.log("===Part 1 Start===");
 
     // Declare two variables, num1 and num2
         let num1 = 10;
@@ -10,8 +11,11 @@
         console.log("The sum of " + num1 + " * " + num2 + " is ", (num1 * num2));
         console.log("The sum of " + num1 + " / " + num2 + " is ", (num1 / num2));
         console.log("The sum of " + num1 + " % " + num2 + " is ", (num1 % num2));
-    
+
+console.log("===Part 1 End===");  
+
 // Part 2: Relational Operators
+console.log("===Part 2 Start===");
 
     // Compare the two variables using relational operators (>, <, >=, <=, ===, !==). Log the results to the console
         console.log(num1 + " is greater than " + num2 + " = ", (num1 > num2));
@@ -21,17 +25,23 @@
         console.log(num1 + " is equal to " + num2 + " = " , (num1 === num2));
         console.log(num1 + " is not equal to " + num2 + " = " , (num1 !== num2));
 
+console.log("===Part 2 End===");
+
 // Part 3: Logical Operators
+console.log("===Part 3 Start===");
 
     // Create two boolean variables
         let isSunny = false;
         let isWeekend = true;
 
-    // Use logical operatos (&&, ||) to check combinations of conditions. Display the results in the console
-        console.log("isSunny and isWeekend = true?: " , (isSunny && isWeekend === false));
+    // Use logical operators (&&, ||) to check combinations of conditions. Display the results in the console
+        console.log("isSunny and isWeekend = true?: " , (isSunny === true && isWeekend === true));
         console.log("isSunny or isWeekend = true?: ", (isSunny === true || isWeekend === true));
-        
+
+console.log("===Part 3 End===");
+
 // Part 4: Assignment Operators
+console.log("===Part 4 Start===");
 
     // Declare a variable score and assign it a value
         let score = 0;
@@ -50,10 +60,15 @@
         score *= 3;
         console.log("Score: ", score);
 
-// Part 5: User Interation
+console.log("===Part 4 End===");
+
+// Part 5: User Interaction
+console.log("===Part 5 Start===");
 
     // Use the prompt method to capture user input for their name and age.
     let userName = prompt("Enter your name");
     let userAge = prompt("Enter your age");
     console.log("User's name: " + userName + " User's age: " + userAge);
+
+console.log("===Part 5 End===");
     
