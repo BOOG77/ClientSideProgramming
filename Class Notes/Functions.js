1e7a79a9f33ab364fad4478bfcc1afe7c988c5d5
BOOG77 <dawsonb14@hotmail.com>
2025-02-11 12:24:03 -0400

    Create Functions.js

---
+++ Class Notes/Functions.js
@@ -0,0 +1,20 @@
+// function
+function helloHello(){
+	document.write("hello hello <br>");
+  document.write("end of hello hello <br>");
+}
+
+helloHello();
+
+function helloAgain(name){
+	// a way of combining the text with variables
+	document.write(`Nice to meet you ${name}!<br>`);
+}
+
+helloAgain("Dawson");
+
+function addition(a, b){
+document.write("addition result is " + (a + b));
+document.write("<br> 🚶 End of Code function 2 🚶");
+}
+addition(3,4);
