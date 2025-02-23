266e61332b0f15583f2faa5baa3515d4deb611b8
Dawson Brown <147319583+BOOG77@users.noreply.github.com>
2025-02-23 19:12:35 -0400

    bugs fixed

--- Exercise 4/script.js
+++ Exercise 4/script.js
@@ -1,14 +1,14 @@
-student_name = Nadia;
-studentAge == "20";
-isEnrolled = True; 
+studentName = "Nadia";  // student_name changed to studentName. meets typical naming conventions, and matches variable on line 6. quotes were missing around the string.
+studentAge = "20"; // == changed to =
+isEnrolled = true; // True changed to true so the datatype is actually a boolean
 
-document.write("<h2>Student Info</h2>")
-document.write("Name: " + studentName + <br>); 
-document.write("Age: " + student_age); 
-document.write("Enrolled: " + isEnrolled) 
+document.write("<h2>Student Info</h2>"); // semicolon missing
+document.write("Name: " + studentName + "<br>"); // quotes missing around <br>
+document.write("Age: " + studentAge); // student_age changed to studentAge
+document.write("Enrolled: " + isEnrolled); // semicolon missing
 
 let ageString = String(studentAge)
-document.write("Age Type: " typeof ageString); 
+document.write("Age Type: ", typeof(ageString)); // missing , after string. brackets were missing in typeof(ageString)
 
 num1 = prompt("Enter first number");
 num2 = prompt("Enter second number");
@@ -17,16 +17,16 @@ total = num1 + num2;
 document.write("Sum: " + total)
 
 userAge = prompt("Enter your age");
-if (userAge = 18) { 
+if (userAge == 18) { // assignment operator changed to comparison(= changed to ==)
     document.write("You can vote!"); 
-} else (userAge < 18) { 
+} else { // unnecessary condition removed
     document.write("Sorry, you can't vote."); 
 }
 
 for (i = 1; i < 10; i++) 
     document.write(i)
 
-while num < 10 
+let num; // variable needs to be assigned outside of the loops scope
+while (num < 10) // missing brackets for condition 
     num = prompt("Enter a number greater than 10")
 
-bing bong
