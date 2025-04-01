703fb6e330a55cacc7d7b9d8df7362a428d8a3b4
BOOG77 <dawsonb14@hotmail.com>
2025-04-01 13:48:38 -0300

    Update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -6,6 +6,10 @@ class Transaction{
         this.type = type;
         this.date = date;
     }
+
+    transactionSummary(){
+        return this.date + " " + this.type + " " + this.category + " " + this.amount;
+    }
 }
 
 class BudgetTracker{
@@ -13,21 +17,56 @@ class BudgetTracker{
         this.balance = 0;
         this.expenseTotal = 0;
         this.incomeTotal = 0;
+        this.transactions = [];
     }
 
-    addExpense(){ 
-        let todayDate = new Date();
-        let expense = new Transaction(prompt("Amount: "), prompt("Income or Expense?: "), prompt("Type: "), todayDate.toDateString());
-        this.balance = this.balance - expense.amount;
-        console.log(expense);
+    addTransaction(Transaction){
+        this.transactions.push(Transaction);
     }
-    addIncome(){
-        balance = transaction.amount + balance;
+
+    showTransactions(){
+        console.log("All transactions: ");
+        for(let i = 0; i < this.transactions.length; i++){
+            console.log(this.transactions[i].transactionSummary());
+        }
     }
-    
+
+    showIncomeTransactions(){
+        console.log("Income transactions: ");
+        for(let i = 0; i < this.transactions.length; i++){
+            if (this.transactions[i].type === "income"){
+                this.incomeTotal += this.transactions[i].amount;
+                console.log(this.transactions[i].transactionSummary());
+            }
+        }
+        console.log("Total income: ", this.incomeTotal);
+        return this.incomeTotal;
+    }
+
+    showExpenseTransactions(){
+        console.log("Expense transactions: ");
+        for(let i = 0; i < this.transactions.length; i++){
+            if (this.transactions[i].type === "expense"){
+                this.expenseTotal += this.transactions[i].amount;
+                console.log(this.transactions[i].transactionSummary());
+            }
+        }
+        console.log("Total expense: ", this.expenseTotal);
+        return this.expenseTotal;
+    }
+
+    showBalance() {
+        return console.log(this.showIncomeTransactions() - this.showExpenseTransactions());
+    }
 };
 
+let todayDate = new Date();
 let budgetTracker = new BudgetTracker();
 
-budgetTracker.addExpense();
-console.log(budgetTracker.balance);
+budgetTracker.addTransaction(new Transaction(5000, "salary", "income", todayDate.toDateString()));
+budgetTracker.addTransaction(new Transaction(2000, "lottery", "income", todayDate.toDateString()));
+budgetTracker.addTransaction(new Transaction(1000, "bad boy fine", "expense", todayDate.toDateString()));
+budgetTracker.showIncomeTransactions();
+budgetTracker.showExpenseTransactions();
+budgetTracker.showTransactions();
+budgetTracker.showBalance();
