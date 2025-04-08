794177c219337a36178c48fa5c5820196066aa23
BOOG77 <dawsonb14@hotmail.com>
2025-04-08 18:54:03 -0300

    assign 2

---
+++ Assignment 2/README.txt
@@ -0,0 +1,5 @@
+This is a barebones budget tracker program.
+Check console for output.
+In this example, we have 3 mock transactions.
+User functionality could be added so they could enter their own transactions.
+But for now, three default transactions have been added into the program, these are displayed in the console.
--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,4 +1,4 @@
-// Define a class that represents a financial transaction.
+// 📌 Create a Transaction Class
 class Transaction{
     constructor(amount, category, type, date){
         this.amount = amount;
@@ -12,6 +12,7 @@ class Transaction{
     }
 }
 
+// 📌 Create a BudgetTracker Class
 class BudgetTracker{
     constructor(){
         this.balance = 0;
@@ -25,48 +26,60 @@ class BudgetTracker{
     }
 
     showTransactions(){
-        console.log("All transactions: ");
+        console.log("=====All transactions=====");
         for(let i = 0; i < this.transactions.length; i++){
             console.log(this.transactions[i].transactionSummary());
         }
+        console.log("==========================");
     }
 
-    showIncomeTransactions(){
-        console.log("Income transactions: ");
+    getIncome(){
+        //console.log("Income transactions: ");
+        this.incomeTotal = 0;
         for(let i = 0; i < this.transactions.length; i++){
             if (this.transactions[i].type === "income"){
                 this.incomeTotal += this.transactions[i].amount;
-                console.log(this.transactions[i].transactionSummary());
+                //console.log(this.transactions[i].transactionSummary());
             }
         }
-        console.log("Total income: ", this.incomeTotal);
+        //console.log("Total income: ", this.incomeTotal);
         return this.incomeTotal;
     }
 
-    showExpenseTransactions(){
-        console.log("Expense transactions: ");
+    getExpenses(){
+        //console.log("Expense transactions: ");
+        this.expenseTotal = 0;
         for(let i = 0; i < this.transactions.length; i++){
             if (this.transactions[i].type === "expense"){
                 this.expenseTotal += this.transactions[i].amount;
-                console.log(this.transactions[i].transactionSummary());
+                //console.log(this.transactions[i].transactionSummary());
             }
         }
-        console.log("Total expense: ", this.expenseTotal);
+        //console.log("Total expense: ", this.expenseTotal);
         return this.expenseTotal;
     }
 
-    showBalance() {
-        return console.log(this.showIncomeTransactions() - this.showExpenseTransactions());
+    getBalance() {
+        return this.getIncome() - this.getExpenses();
+    }
+
+    accountDetails(){
+        console.log("Total expense: ", this.getExpenses());
+        console.log("Total income: ", this.getIncome());
+        console.log("Current balance: ", this.getBalance());
     }
 };
 
 let todayDate = new Date();
+
+// 📌 Create an instance of the BudgetTracker class.
 let budgetTracker = new BudgetTracker();
 
+// 📌 Add at least three transactions: One income (e.g., Salary). Two expenses (e.g., Food, Shopping).
 budgetTracker.addTransaction(new Transaction(5000, "salary", "income", todayDate.toDateString()));
-budgetTracker.addTransaction(new Transaction(2000, "lottery", "income", todayDate.toDateString()));
+budgetTracker.addTransaction(new Transaction(2000, "late rent", "expense", todayDate.toDateString()));
 budgetTracker.addTransaction(new Transaction(1000, "bad boy fine", "expense", todayDate.toDateString()));
-budgetTracker.showIncomeTransactions();
-budgetTracker.showExpenseTransactions();
+
+// 📌 Display all transactions in the console or UI.
 budgetTracker.showTransactions();
-budgetTracker.showBalance();
+budgetTracker.accountDetails();
